f74e079aeddf54317bd775ce513f99b168ad6aaa
fahad <fahadmahmud225@gmail.com>
2024-02-01 09:41:30 +0600

    added dashboard link to mobile navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,25 +17,23 @@ const layout: React.FC<layoutProps> = async ({ children }) => {
   }
 
   return (
-    <>
-      <div className="flex">
-        <SlideMenuContainer />
-        {/* the vertical navigation bar */}
-        <div className="hidden sm:block fixed top-0 left-0 h-full">
-          <DashBoardNavbar />
-        </div>
-
-        {/* the horizontal navbar */}
-        <div className="sm:hidden">
-          <HorizontalNavbar />
-        </div>
-
-        {/* the Dashboard */}
-        <div className="pt-14 sm:pt-0 sm:pl-[5.5rem] w-full max-w-[1600px] min-h-screen mx-auto">
-          {children}
-        </div>
+    <div className="flex">
+      <SlideMenuContainer />
+      {/* the vertical navigation bar */}
+      <div className="hidden sm:block fixed top-0 left-0 h-full">
+        <DashBoardNavbar />
       </div>
-    </>
+
+      {/* the horizontal navbar */}
+      <div className="sm:hidden">
+        <HorizontalNavbar />
+      </div>
+
+      {/* the Dashboard */}
+      <div className="pt-14 sm:pt-0 sm:pl-[5.5rem] w-full max-w-[1600px] min-h-screen mx-auto">
+        {children}
+      </div>
+    </div>
   );
 };
 
--- src/components/HorizontalNavbar.tsx
+++ src/components/HorizontalNavbar.tsx
@@ -7,15 +7,24 @@ import OpenSlideMuneButton from "./OpenSlideMenuButton";
 const HorizontalNavbar: React.FC = ({}) => {
   return (
     <div className="flex justify-between items-center w-full h-14 fixed top-0 left-0 px-4 bg-green-500 z-50">
-      <Link href="/">
-        <Image
-          src={Logo}
-          alt="logo"
-          width={96}
-          height={96}
-          className="w-12 select-none"
-        />
-      </Link>
+      <div className="flex items-center gap-x-1">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="logo"
+            width={96}
+            height={96}
+            className="w-12 select-none"
+          />
+        </Link>
+
+        <Link
+          href={"/dashboard"}
+          className="text-xl font-semibold text-white hover:text-green-100"
+        >
+          Dashboard
+        </Link>
+      </div>
 
       <OpenSlideMuneButton />
     </div>
